b60bc3ac842b59c6ced4d2545c6796240c5aca1f
sandra <sandrav0609@gmail.com>
2018-11-24 11:21:20 -0400

    Add README and 5 anime to database.

--- AniBot.pl
+++ AniBot.pl
@@ -1,4 +1,4 @@
-anime(X) :- member(X,["Dragon Ball", "Naruto", "Bleach", "HunterXHunter", "Hamtaro", "Full Metal Alchemist"]).
+anime(X) :- member(X,["Dragon Ball", "Naruto", "Bleach", "HunterXHunter", "Hamtaro", "Full Metal Alchemist", "Attack on Titan", "Neon Genesis Evangelion", "Elfen Lied", "Inuyasha", "Blue Exorcist"]).
 
 genero(X) :- member(X,["Aventura", "Shoujo", "Shounen", "Kodomo", "Seinen", "Josei", "Ficción",
                     "Fantasía", "Mecha", "Sobrenatural", "Magia", "Gore"]).
@@ -10,6 +10,12 @@ generoAnime("HunterXHunter",["Seinen", "Aventura"]).
 generoAnime("Hamtaro",["Kodomo"]).
 generoAnime("Full Metal Alchemist",["Shounen", "Magia"]).
 
+generoAnime("Attack on Titan",["Seinen", "Aventura"]).
+generoAnime("Neon Genesis Evangelion",["Seinen", "Mecha"]).
+generoAnime("Elfen Lied",["Ficción", "Gore"]).
+generoAnime("Inuyasha",["Shoujo", "Aventura", "Fantasía"]).
+generoAnime("Blue Exorcist",["Ficción", "Fantasía"]).
+
 rating("Dragon Ball",3).
 rating("Naruto",1).
 rating("Bleach",4).
@@ -17,9 +23,23 @@ rating("HunterXHunter",5).
 rating("Hamtaro",2).
 rating("Full Metal Alchemist",4).
 
+rating("Attack on Titan",5).
+rating("Neon Genesis Evangelion",4).
+rating("Elfen Lied",3).
+rating("Inuyasha",4).
+rating("Blue Exorcist",2).
+
 popularidad("Dragon Ball",7).
 popularidad("Naruto",5).
 popularidad("Bleach",8).
 popularidad("HunterXHunter",3).
 popularidad("Hamtaro",10).
 popularidad("Full Metal Alchemist",1).
+
+popularidad("Attack on Titan",10).
+popularidad("Neon Genesis Evangelion",6).
+popularidad("Elfen Lied",4).
+popularidad("Inuyasha",9).
+popularidad("Blue Exorcist",2).
+
+
---
+++ README.txt
@@ -0,0 +1,12 @@
+Integrantes:
+    Aurivan Castro  14-10205
+    Sandra Vera     14-11130
+
+Oraciones con las cuales funcionan los predicados de AniBot:
+	-Para mostrar animes por género
+
+	-Para mostrar animes por estrellas
+
+	-Para mostrar animes poco conocidos
+	
+	-Para agregar animes
