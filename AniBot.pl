1a611a7f236086a600e674e9d8ca128fa30a4bfb
sandra <sandrav0609@gmail.com>
2018-11-24 10:48:02 -0400

    First commit

---
+++ AniBot.pl
@@ -0,0 +1,25 @@
+anime(X) :- member(X,["Dragon Ball", "Naruto", "Bleach", "HunterXHunter", "Hamtaro", "Full Metal Alchemist"]).
+
+genero(X) :- member(X,["Aventura", "Shoujo", "Shounen", "Kodomo", "Seinen", "Josei", "Ficción",
+                    "Fantasía", "Mecha", "Sobrenatural", "Magia", "Gore"]).
+
+generoAnime("Naruto",["Shounen","Aventura"]).
+generoAnime("Dragon Ball",["Shounen"]).
+generoAnime("Bleach",["Shounen", "Sobrenatural"]).
+generoAnime("HunterXHunter",["Seinen", "Aventura"]).
+generoAnime("Hamtaro",["Kodomo"]).
+generoAnime("Full Metal Alchemist",["Shounen", "Magia"]).
+
+rating("Dragon Ball",3).
+rating("Naruto",1).
+rating("Bleach",4).
+rating("HunterXHunter",5).
+rating("Hamtaro",2).
+rating("Full Metal Alchemist",4).
+
+popularidad("Dragon Ball",7).
+popularidad("Naruto",5).
+popularidad("Bleach",8).
+popularidad("HunterXHunter",3).
+popularidad("Hamtaro",10).
+popularidad("Full Metal Alchemist",1).
